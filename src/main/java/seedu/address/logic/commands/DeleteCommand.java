7e939ef4a514caae9d85ca3aead1c7e238efb863
mistyk786 <47024210+mistyk786@users.noreply.github.com>
2025-03-16 02:38:54 +0800

    Update DeleteCommand.java

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -18,9 +18,9 @@ public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+    public static final String MESSAGE_USAGE = String.format("%s: Deletes the person identified by the index number "
+        + "used in the displayed person list.%nParameters: INDEX (must be a positive integer)%nExample: %s 1",
+        COMMAND_WORD, COMMAND_WORD);
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
     public static final String MESSAGE_CONFIRMATION = "Are you sure you want to delete this patient? (y/n)";
