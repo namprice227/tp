8b6f57b84ddf38c1569a8d36cc11d7fd62c56c4c
Mustafa Abbas <mistyk786@gmail.com>
2025-03-16 21:44:52 +0800

    Update DeleteCommand

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -18,7 +18,8 @@ public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes the person identified by the index number used in the displayed person list.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes the person identified by the index number used"
+            + " in the displayed person list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
@@ -29,6 +30,10 @@ public class DeleteCommand extends Command {
     private Person personToDelete;
     private boolean needsConfirmation;
 
+    /**
+     * Constructor
+     * @param targetIndex
+     */
     public DeleteCommand(Index targetIndex) {
         requireNonNull(targetIndex);
 
