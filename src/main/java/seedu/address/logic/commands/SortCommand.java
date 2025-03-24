cc859330da4ac66a1740d1e74fe0fd31da668ab3
Paul Cheng <85998077+itsapaulblem@users.noreply.github.com>
2025-03-24 10:36:00 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -9,45 +9,45 @@ import seedu.address.model.Model;
  * Sorts all persons in the address book by the specified field.
  */
 public class SortCommand extends Command {
-  public static final String COMMAND_WORD = "sort";
+    public static final String COMMAND_WORD = "sort";
 
-  public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts all patients in the address book "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts all patients in the address book "
           + "by the specified field.\n"
           + "Parameters: FIELD (must be either \"name\" or \"appointment\")\n"
           + "Example: " + COMMAND_WORD + " name";
 
-  public static final String MESSAGE_SUCCESS = "Sorted all patients by %s";
+    public static final String MESSAGE_SUCCESS = "Sorted all patients by %s";
 
-  private final String sortField;
+    private final String sortField;
 
-  public SortCommand(String sortField) {
-    this.sortField = sortField;
-  }
-
-  @Override
-  public CommandResult execute(Model model) {
-    requireNonNull(model);
-
-    switch (sortField.toLowerCase()) {
-    case "name":
-      model.sortPersonListByName();
-      break;
-    case "appointment":
-      model.sortPersonListByAppointment();
-      break;
-    default:
-      // This should not happen as parser should validate input
-      throw new AssertionError("Unknown sort field: " + sortField);
+    public SortCommand(String sortField) {
+        this.sortField = sortField;
     }
 
-    model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-    return new CommandResult(String.format(MESSAGE_SUCCESS, sortField));
-  }
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
 
-  @Override
-  public boolean equals(Object other) {
-    return other == this
+        switch (sortField.toLowerCase()) {
+        case "name":
+            model.sortPersonListByName();
+            break;
+        case "appointment":
+            model.sortPersonListByAppointment();
+            break;
+        default:
+            // This should not happen as parser should validate input
+            throw new AssertionError("Unknown sort field: " + sortField);
+    }
+
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, sortField));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
             || (other instanceof SortCommand // instanceof handles nulls
             && sortField.equals(((SortCommand) other).sortField)); // state check
-  }
+    }
 }
