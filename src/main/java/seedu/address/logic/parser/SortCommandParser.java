d46b27c3189a7761f2ea7d0b2d76a39857727f81
Paul Cheng <85998077+itsapaulblem@users.noreply.github.com>
2025-03-24 10:46:00 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -10,11 +10,11 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class SortCommandParser implements Parser<SortCommand> {
 
-   /**
-   * Parses the given {@code String} of arguments in the context of the SortCommand
-   * and returns a SortCommand object for execution.
-   * @throws ParseException if the user input does not conform to the expected format.
-   */
+    /**
+    * Parses the given {@code String} of arguments in the context of the SortCommand
+    * and returns a SortCommand object for execution.
+    * @throws ParseException if the user input does not conform to the expected format.
+    */
     @Override
     public SortCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
