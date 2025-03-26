61f87e745705e3895da89902590501941de51ea1
Paul Cheng <85998077+itsapaulblem@users.noreply.github.com>
2025-03-26 20:17:58 +0800

    Update UndoCommandParser.java

--- src/main/java/seedu/address/logic/parser/UndoCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UndoCommandParser.java
@@ -11,27 +11,27 @@ import seedu.address.model.Model;
  * Parses input arguments and creates a new UndoCommand object
  */
 public class UndoCommandParser implements Parser<UndoCommand> {
-  private final Model model;
+    private final Model model;
 
-  public UndoCommandParser(Model model) {
-    this.model = model;
-  }
+    public UndoCommandParser(Model model) {
+        this.model = model;
+    }
 
   /**
    * Parses the given {@code String} of arguments in the context of the UndoCommand
    * and returns an UndoCommand object for execution.
    * @throws ParseException if the user input does not conform to the expected format
    */
-  public UndoCommand parse(String args) throws ParseException {
-    requireNonNull(args);
+    public UndoCommand parse(String args) throws ParseException {
+      requireNonNull(args);
 
-    // Trim and check for any unexpected arguments
-    if (!args.trim().isEmpty()) {
-      throw new ParseException(
+        // Trim and check for any unexpected arguments
+        if (!args.trim().isEmpty()) {
+            throw new ParseException(
               String.format(MESSAGE_INVALID_COMMAND_FORMAT, UndoCommand.MESSAGE_USAGE)
       );
-    }
+        }
 
-    return new UndoCommand(model);
-  }
+        return new UndoCommand(model);
+    }
 }
