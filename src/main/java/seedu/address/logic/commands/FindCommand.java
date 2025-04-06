bc5b0568813dd867424a0a30319525640a3e2efb
namprice227 <e1249226@u.nus.edu>
2025-04-07 03:10:14 +0800

    Fix Find

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -29,11 +29,12 @@ public class FindCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        model.setLastCommandArchiveRelated(false);
         if (model.isArchiveMode()) {
             model.updateArchivedFilteredPersonList(predicate);
             return new CommandResult(String.format(
-                    Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredArchivedPersonList().size())
-            );
+                    Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredArchivedPersonList().size()),
+                    CommandResult.ListType.ARCHIVE);
         }
         model.updateFilteredPersonList(predicate);
         return new CommandResult(
